517f261805dd055ba43e624ff17adc4f3ebedecb
Uchechukwu Udemezue <udemezue008@gmail.com>
2024-01-11 15:12:06 +0100

    minor issues

--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='core/index.html')),
-    path('', views.homepage, name =''),
-    path('register',views.register, name='register'),
-    path('my-login', views.my_login, name='my-login'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('user-logout', views.user_logout, name="user-logout"),
+    path('', TemplateView.as_view(template_name='core/index.html')),
+    # path('', views.homepage, name =''),
+    # path('register',views.register, name='register'),
+    # path('my-login', views.my_login, name='my-login'),
+    # path('dashboard', views.dashboard, name='dashboard'),
+    # path('user-logout', views.user_logout, name="user-logout"),
 
 
 
